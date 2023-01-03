d44e59dd3202bf7fdc419ebea7bd311ccc231959
Garner Deng <56040329+ggarnerdeng@users.noreply.github.com>
2023-01-02 16:17:30 -0800

    Reference manipulation by comparative int function

--- Training/CSCI123_3_12-refAndPoint.cpp
+++ Training/CSCI123_3_12-refAndPoint.cpp
@@ -8,21 +8,67 @@ vector<int> squareAllElements(vector<int> v);
 void cleanse(vector<int> vs);
 void purge(vector<int>& vs);
 void p4(int& a);
+void swapInts(int& a, int& b);
+void compareThenSwap(int& a, int& b);
+void organizeVector(vector<int>& vs);
+
 int main(){
 
     vector<int> v1 = {1,2,3,4,5,6,7,8,9};
+    //vector<int> v2 = {9,8,7,6,5,4,3,2,1,11,12,13,454,2,3,4};
+    vector<int> v2 = {4,3,2,1,100,5};
+
     /*
     v1 = squareAllElements(v1);
     purge(v1);
     for(int e : v1)
         cout<<e<<" ";
-*/
+
     int a = 12413;
-   
     p4(a);
     cout<<"final a is "<<a;
+
+    int a = 1;
+    int b = 2;
+    swapInts(a,b);
+    cout<<a<<" "<<b;
+
+    int a = 12;
+    int b = 2;
+    compareThenSwap(a,b);
+    cout<<a<<" "<<b;
+*/
+    organizeVector(v2);
+    //RANGED-BASED FOR LOOP:::
+    for(int e : v2)
+        cout<<" "<<e;
+    
     return 0;
 }
+
+void organizeVector(vector<int>& vs){
+    for(int i = 0; i < vs.size(); i++){
+        for(int j = 0; j < (vs.size()-1-i); j++){
+            int k = j + 1;
+            compareThenSwap(vs.at(j),vs.at(k));
+            //cout<<"j is: "<<j<<" and k is "<<k<<endl;
+        }
+    }
+}
+
+void compareThenSwap(int& a, int& b){
+    if(a>b){
+        swapInts(a,b);
+    }
+}
+
+void swapInts(int& a, int& b){
+    int temporary;
+    temporary = a;
+    a = b;
+    b = temporary;
+}
+
 
 void p4(int& a){
      while(a!=1){
@@ -36,10 +82,13 @@ void p4(int& a){
      }
    
 }
+
+//Does not do anything, because "vs" is passed as a copy.
 void cleanse(vector<int> vs){
  vs.clear();
 
 }
+//Actually clears, because the REFERENCE of "vs" is passed. 
 void purge(vector<int>& vs){
  vs.clear();
 
@@ -50,3 +99,4 @@ vector<int> squareAllElements(vector<int> v){
     }
     return v;
 }
+
